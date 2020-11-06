e9ae464f0c55d6f7775712d3994b4c79c77af3f6
Gabi <gabee_88@yahoo.com>
2020-11-06 09:45:57 +0100

    Added Hooks and finished the tutorial

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,41 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import Loader from '../Components/Loader'
+import { useAxiosGet } from '../Hooks/HttpRequests'
 
 function Product(){
     const { id } = useParams()
     const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
-    const [product, setProduct] = useState({
-        loading: false,
-        data: null,
-        error: false
-    })
+    
+    let product = useAxiosGet(url)
 
     let content = null
-
-    useEffect(() => {
-        setProduct({
-            loading: true,
-            data: null,
-            error: false
-        })
-        axios.get(url)
-            .then(response => {
-                setProduct({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch(() => {
-                setProduct({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            })
-    }, [url])
 
     if(product.error){
         content = <p>
